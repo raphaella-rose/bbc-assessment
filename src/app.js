7417e0cc4831c547c7cea836ac2b16bf866f390d
Raphaella <raphaella@Raphaellas-MacBook-Air.local>
2022-06-18 17:49:08 +0100

    removed npm axios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 function printApi(response) {
-  console.log(response)
+  console.log(response.data)
 }
 
 
 function api_test() {
   let apiKey = "6841a8f6eab8ec70512df6bf3950a0cd51d4f9b7373b9df5";
-  let apiUrl = "https://jse-assignment.herokuapp.com/USElection/states/";
+  let apiUrl = "https://jse-assignment.herokuapp.com/USElection/presidential/stateresults/";
   axios.get(apiUrl, {
     headers: {
       'x-api-key': apiKey
