a4f7c29ceaa066e501ad433590a09ef87c8a3649
Raphaella <raphaella@Raphaellas-MacBook-Air.local>
2022-06-18 17:01:44 +0100

    api setup

---
+++ src/app.js
@@ -0,0 +1,17 @@
+function printApi(response) {
+  console.log(response)
+}
+
+
+function api_test() {
+  let apiKey = "6841a8f6eab8ec70512df6bf3950a0cd51d4f9b7373b9df5";
+  let apiUrl = "https://jse-assignment.herokuapp.com/USElection/states/";
+  axios.get(apiUrl, {
+    headers: {
+      'x-api-key': apiKey
+    }
+  }).then(printApi)
+}
+
+
+api_test()
