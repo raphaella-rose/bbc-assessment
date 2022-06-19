312d9d1febd37b4f2bae7b260f6c0748ad4d2bcc
Raphaella <raphaella@Raphaellas-MacBook-Air.local>
2022-06-19 12:33:47 +0100

    state selector working for state name

--- src/app.js
+++ src/app.js
@@ -3,13 +3,23 @@ function electionData() {
   let stateNameElement = document.querySelector("#state-name");
   let winningPartyElement = document.querySelector("#winning-party");
 
-
-
-  function printStateName(response) {
-    stateNameElement.innerHTML = (response.data.states[0].state.localisedItems[1].longName);
+  function chooseState(response, name) {
+    states = response.data.states;
+    for (let i = 0; i < states.length; i++) {
+      if (states[i].state.localisedItems[1].longName == name) {
+        return i;
+      }
+    };
+  
   }
 
+  function printStateName(response, stateCode) {
+    stateNameElement.innerHTML = (response.data.states[stateCode].state.localisedItems[1].longName);
+  }
+
+
   function printPartyResults(response) {
+    
     results = response.data.stateResults[0].stateResult.resultItems;
     let resultsElement = document.querySelector("#results");
     let resultsHTML = "";
@@ -22,8 +32,7 @@ function electionData() {
       `<li>${partyName} party</li>
       <li>${votes} votes</li>
       <li>${votesAsPercentage}%</li>`;
-      console.log(votes)
-      console.log(votesAsPercentage)
+   
       
     })
     resultsElement.innerHTML = resultsHTML;
@@ -55,7 +64,6 @@ function electionData() {
 
   }
 
-
   function api_test(call, methodName) {
    
     let apiKey = "6841a8f6eab8ec70512df6bf3950a0cd51d4f9b7373b9df5";
@@ -66,11 +74,17 @@ function electionData() {
       }
     }).then(methodName)
   }
+  
 
 
-  api_test("states/", printStateName)
+  api_test("states/", dataController)
   api_test("presidential/stateresults/", getPartyName)
+  
 
+  function dataController(response) {
+    stateCode = chooseState(response, "Alaska");
+    printStateName(response, stateCode);
+  }
 
 }
 
